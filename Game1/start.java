5332d09e9c0130c91e905197758687d861d61eb8
afakjfghakhdfkaj <122028664+afakjfghakhdfkaj@users.noreply.github.com>
2023-01-05 21:58:38 +0800

    Update start.java

--- Game1/start.java
+++ Game1/start.java
@@ -1,1 +1,17 @@
+package Game1;
 
+public class start{
+    public static void main(String[] args){
+        paneMap m1 = new paneMap();
+        paneMap m2 = new paneMap();
+        m1.set(0,0,0);
+        m1.set(0,1,0);
+        m1.set(1,0,2);
+        m1.set(1,1,2);
+        m2.set(0,0,"abc");
+        m2.set(0,1,"def");
+        m2.set(1,0,"试试");
+        m2.set(1,1,"汉字");
+        display.out(6,m1,m2,2,2);
+    }
+}
